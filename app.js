da6dedd8e232c72b77541381852865be6212316e
Pradeep-Prady <wolferstechdev@gmail.com>
2024-05-04 14:49:09 +0530

    prod  test 7

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-dotenv.config({ path: path.join(__dirname, "./.env") });
+// dotenv.config({ path: path.join(__dirname, "./.env") });
 
 console.log(process.env.PORT, "env in app");
 
--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from 'url';
 
-// dotenv.config({ path: `./${process.env.NODE_ENV}.env` });
+dotenv.config({ path: `./${process.env.NODE_ENV}.env` });
 // dotenv.config({ path: `./.env` });
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, "../../.env") });
+// dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 
 // console.log( `./${process.env.NODE_ENV}.env`)
