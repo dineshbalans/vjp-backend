61a582477bed70ff4d1e1e9f32ec2bde86fa785c
Pradeep-Prady <wolferstechdev@gmail.com>
2024-05-04 13:23:02 +0530

    prod updates 3test

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
-dotenv.config({ path: `./${process.env.NODE_ENV}.env` });
+// dotenv.config({ path: `./${process.env.NODE_ENV}.env` });
+dotenv.config({ path: `./.env` });
+
 
 
 console.log( `./${process.env.NODE_ENV}.env`)
