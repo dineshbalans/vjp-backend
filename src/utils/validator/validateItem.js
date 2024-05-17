ad09bf440e83f5c10264003504e11f4988b6fdb7
Pradeep-Prady <pradeepprady005@gmail.com>
2024-05-17 16:19:26 +0530

    item create 5

--- src/utils/validator/validateItem.js
+++ src/utils/validator/validateItem.js
@@ -12,5 +12,5 @@ export const validateCreateItem = Joi.object().keys({
     subCategoryId: Joi.string().optional(),
     isSale: Joi.boolean().optional(),
     isTrending: Joi.boolean().optional(),
-    highlights: Joi.object().optional(),
+    highlights: Joi.optional(),
 });
